143c2f29cb3b8ab46bbe0029a9d408ba6e12c044
khj02021 <khj02021@gmail.com>
2018-06-02 00:29:52 +1000

    SceneManager Script
    SceneManager Script for portal

---
+++ Assets/Scripts/Portal3to4.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;  // To manage scene load
+using UnityEngine;
+
+public class Portal3to4 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Load Level4 scene when collides
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Portal prefab")
+        {
+            SceneManager.LoadScene("Level4");
+        }
+    }
+}
---
+++ Assets/Scripts/Portal4to5.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;  // To manage scene load
+using UnityEngine;
+
+public class Portal4to5 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Load Level5 scene when collides
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Portal prefab")
+        {
+            SceneManager.LoadScene("Level5");
+        }
+    }
+}
