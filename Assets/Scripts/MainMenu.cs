45f221821d48e20fd4e51adf421f55ef1b1c8dcc
DuyNguyen <aro2181991@gmail.com>
2018-06-01 21:01:36 +1000

    Menu(done)
    Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu2 : MonoBehaviour
+{
+    public void Playgame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame(){
+        Debug.Log("Quit");
+        Application.Quit();
+
+    }
+}
